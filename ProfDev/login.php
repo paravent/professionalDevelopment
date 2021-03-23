857b705bb690b1b2283e89317612c6360367d197
paravent <58010504+paravent@users.noreply.github.com>
2021-03-23 17:55:01 +0000

    Update login.php

--- ProfDev/login.php
+++ ProfDev/login.php
@@ -64,7 +64,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: index.php");
+                            header("location: index.php?show_id=s1004");
                             
                         } else{
                             // Display an error message if password is not valid
